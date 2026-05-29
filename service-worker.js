a750376be3121ce744d33615e568b8cf3b656700
ebmjosedovallejpfpolisofc <mfpd.central.ebmjvp@gmail.com>
2026-05-29 00:01:31 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,48 +1,107 @@
-const CACHE_NAME = "spjvp-scanner-v1";
+const CACHE_NAME =
+"sgjvp-cache-v1";
 
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+const urlsToCache = [
+
+"./",
+"./index.html",
+"./portal-aluno.html",
+"./portal-monitor-prof.html",
+"./manifest.json"
+
 ];
 
-// instalar cache
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(FILES_TO_CACHE);
-      })
-  );
+self.addEventListener(
 
-  self.skipWaiting();
-});
+"install",
 
-// ativar
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
+(event)=>{
 
-  self.clients.claim();
-});
+event.waitUntil(
 
-// buscar arquivos
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        return response || fetch(event.request);
-      })
-  );
-});
+caches.open(
+CACHE_NAME
+)
+
+.then(cache=>{
+
+return cache.addAll(
+urlsToCache
+);
+
+})
+
+);
+
+}
+
+);
+
+self.addEventListener(
+
+"fetch",
+
+(event)=>{
+
+event.respondWith(
+
+caches.match(
+event.request
+)
+
+.then(response=>{
+
+return (
+response
+||
+fetch(
+event.request
+)
+);
+
+})
+
+);
+
+}
+
+);
+
+self.addEventListener(
+
+"activate",
+
+(event)=>{
+
+event.waitUntil(
+
+caches.keys()
+
+.then(keys=>{
+
+return Promise.all(
+
+keys.map(key=>{
+
+if(
+key !==
+CACHE_NAME
+){
+
+return caches.delete(
+key
+);
+
+}
+
+})
+
+);
+
+})
+
+);
+
+}
+
+);
